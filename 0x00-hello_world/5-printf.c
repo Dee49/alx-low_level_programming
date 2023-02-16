f50374e914c6994fd95d3ec9300b513901ac8bd6
Dibuseng Ramosoelu <ramosoeludibuseng@gmail.com>
2023-02-16 06:45:27 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,6 @@
  * main - A program that print a line using the printf function
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	pritf("with proper grammar, but the outcome is a piece of art,\n");
